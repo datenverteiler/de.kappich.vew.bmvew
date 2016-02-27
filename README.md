bb1598a5fef4e78f0facd2ce5613335709034a2b
Falko Schumann <falko.schumann@muspellheim.de>
2016-02-27 22:14:30 +0100

    Projekt mit dem Code von Kernsoftware v3.6.5 initialisiert

---
+++ README.md
@@ -0,0 +1,11 @@
+de.kappich.vew.bmvew v${project.version} [![Build Status](https://travis-ci.org/datenverteiler/de.kappich.vew.bmvew.svg?branch=master)](https://travis-ci.org/datenverteiler/de.kappich.vew.bmvew)
+===========================
+
+
+Bauen und Veröffentlichen
+-------------------------
+
+    install site assembly:single
+
+Im Ordner `target` wird damit das Distributionspaket
+`SWE_{Name}_{Status}_V{Version}_D{Standdatum}.zip` erstellt.
---
+++ src/main/java/de/kappich/vew/bmvew/PackageRuntimeInfo.java
@@ -0,0 +1,14 @@
+
+            package de.kappich.vew.bmvew;
+            public class PackageRuntimeInfo {
+            public static String getLicence() { return "GPL";}
+            public static String getRelease() { return "Kernsoftware 3.6.5";}
+            public static String getVersion() { return "3.6.5";}
+            public static String getRevision() { return "13522";}
+            public static String getCompileTime() { return "21.05.2015 19:18:49";}
+            public static String getDependsOnCompiled() { return "de.bsvrz.dav.daf, de.bsvrz.sys.funclib.dataSerializer, de.bsvrz.sys.funclib.communicationStreams, de.bsvrz.sys.funclib.debug, de.bsvrz.sys.funclib.commandLineArgs, de.bsvrz.sys.funclib.application";}
+            public static String getDependsOnSource() { return "";}
+            public static String getDependsOnLib() { return "";}
+            public static String getJvmVersion() { return "1.6";}
+            }
+        
